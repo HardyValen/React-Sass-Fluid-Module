ed6b3d9cc8fe511b3a50868714cd594a3fdfb004
HardyValen <hardyvalen@gmail.com>
2020-06-07 20:34:49 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'normalize.css';
+import './css/main.scss';
+
+function App() {
+    return (
+        <div className="App">
+        <div className="button-primary">Test</div>
+        <div className="button-secondary">Test</div>
+        <div className="button-primary-filled">Test</div>
+        <div className="button-secondary-filled">Test</div>
+        <h1>Test</h1>
+        </div>
+    );
+}
+
+export default App;
